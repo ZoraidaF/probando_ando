01550409530ad50b1d6d9ec74e420f57b90d3cfe
zoraida <z__flores@hotmail.com>
2019-04-27 23:33:11 -0300

    elimino archivo hola.sh

---
+++ archivoBasura.sh
@@ -0,0 +1,16 @@
+
+esto
+solo
+es una 
+prueba para 
+probar 
+cosas de ocurrencias 
+de sistemas operativos
+de la facultad de ingenieria
+
+while read -r linea 
+do
+	echo $linea
+
+don <archivo_salidaDeBasura
+
